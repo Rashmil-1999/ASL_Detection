c31bfb29b01a51d552e8f0c6dbb596ab7e61a6b2
Rashmil Panchani <32737711+Rashmil-1999@users.noreply.github.com>
2019-12-21 23:33:52 +0530

    changed

--- src/detector.py
+++ src/detector.py
@@ -18,6 +18,7 @@ labels = [
     "H",
     "I",
     "J",
+    "K",
     "L",
     "M",
     "N",
@@ -42,15 +43,15 @@ _model = load_model(model_path)
 model_input_size = _model.input_shape[1:3]
 cap = cv2.VideoCapture(0)
 
-x_mid = cap.get(3) / 2
-y_mid = cap.get(4) / 2
+x_mid = int(cap.get(3) / 2)
+y_mid = int(cap.get(4) / 2)
 
 while True:
     ret_val, frame = cap.read()
     if ret_val == True:
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         detection_region = gray[
-            (y_mid - 100) : (y_mid + 100), (x_mid - 100) : (x_mid + 100)
+            (y_mid - 200) : (y_mid + 200), (x_mid - 200) : (x_mid + 200)
         ]
         detection_region = cv2.resize(
             detection_region, model_input_size, interpolation=cv2.INTER_CUBIC
@@ -66,22 +67,22 @@ while True:
 
         cv2.rectangle(
             frame,
-            ((x_mid - 100), (y_mid - 100)),
-            ((x_mid + 100), (y_mid + 100)),
+            ((x_mid - 200), (y_mid - 200)),
+            ((x_mid + 200), (y_mid + 200)),
             (0, 0, 255),
             3,
         )
         cv2.putText(
             frame,
-            labels[emotion_label],
-            ((x_mid - 100), (y_mid - 100)),
+            labels[label],
+            ((x_mid - 200), (y_mid - 200)),
             cv2.FONT_HERSHEY_COMPLEX,
-            4,
+            2,
             (0, 255, 0),
-            10,
+            5,
         )
         cv2.imshow("recognition", frame)
-        if cv2.waitKey(1) == 27:
+        if cv2.waitKey(1) & 0xFF == ord("q"):
             break
 
 cv2.destroyAllWindows()
